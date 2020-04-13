9bea15b8b6bdc435956157504541421c01598a0e
Bryan M <allen.bry95@gmail.com>
2020-04-13 10:11:14 +0200

    :art: Form visuals on Mobile and Desktop

--- oreijin-frontend/src/components/Visitor/Register/index.js
+++ oreijin-frontend/src/components/Visitor/Register/index.js
@@ -1,6 +1,6 @@
 // == Import npm
 import React from 'react';
-import { Container, Form } from 'semantic-ui-react';
+import { Header, Container, Segment, Form } from 'semantic-ui-react';
 // == Import
 
 import './styles.scss';
@@ -8,60 +8,67 @@ import './styles.scss';
 // == Composant
 const Register = () => (
   <Container>
-    <Form>
-      <Form.Field required>
-        <Form.Input
-          label="Prénom"
-          placeholder="Prénom"
-        />
-      </Form.Field>
-      <Form.Field required>
-        <Form.Input
-          label="Nom"
-          placeholder="Nom"
-        />
-      </Form.Field>
-      <Form.Field required>
-        <Form.Input
-          label="Pseudo"
-          placeholder="Pseudo"
-        />
-      </Form.Field>
-      <Form.Field required>
-        <Form.Input
-          label="Date de naissance"
-          placeholder="Date de naissance"
-        />
-      </Form.Field>
-      <Form.Field required>
-        <Form.Input
-          label="Email"
-          placeholder="Email"
-        />
-      </Form.Field>
-      <Form.Field required>
-        <Form.Input
-          label="Mot de passe"
-          placeholder="Mot de passe"
-        />
-      </Form.Field>
-      <Form.Field required>
-        <Form.Input
-          label="Vérification du mot de passe"
-          placeholder="Vérification du mot de passe"
-        />
-      </Form.Field>
-      <Form.Field required>
-        <Form.Input
-          label="Adresse"
-          placeholder="Adresse"
-        />
-      </Form.Field>
-      <Form.Field required>
-        <Form.Checkbox label="I agree to the Terms and Conditions" />
-      </Form.Field>
-      <Form.Button type="submit">Soumettre</Form.Button>
-    </Form>
+    <Segment raised>
+      <Header as="h1" dividing textAlign="center">Inscription</Header>
+      <Form>
+        <Form.Group>
+          <Form.Field required width={8}>
+            <Form.Input
+              label="Prénom"
+              placeholder="Prénom"
+            />
+          </Form.Field>
+          <Form.Field required width={8}>
+            <Form.Input
+              label="Nom"
+              placeholder="Nom"
+            />
+          </Form.Field>
+        </Form.Group>
+        <Form.Group>
+          <Form.Field required width={10}>
+            <Form.Input
+              label="Pseudo"
+              placeholder="Pseudo"
+            />
+          </Form.Field>
+          <Form.Field required width={6}>
+            <Form.Input
+              label="Date de naissance"
+              placeholder="Date de naissance"
+            />
+          </Form.Field>
+        </Form.Group>
+        <Form.Field required>
+          <Form.Input
+            label="Email"
+            placeholder="Email"
+          />
+        </Form.Field>
+        <Form.Field required>
+          <Form.Input
+            label="Mot de passe"
+            placeholder="Mot de passe"
+          />
+        </Form.Field>
+        <Form.Field required>
+          <Form.Input
+            label="Vérification du mot de passe"
+            placeholder="Vérification du mot de passe"
+          />
+        </Form.Field>
+        <Form.Field required>
+          <Form.Input
+            label="Adresse"
+            placeholder="Adresse"
+          />
+        </Form.Field>
+        <Form.Field required>
+          <Form.Checkbox label="I agree to the Terms and Conditions" />
+        </Form.Field>
+        <Form.Button type="submit">Soumettre</Form.Button>
+      </Form>
+    </Segment>
   </Container>
 );
 
