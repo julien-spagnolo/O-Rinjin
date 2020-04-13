ff6f866b7869b99cfc57a55d90991e57ac1d809e
Bryan M <allen.bry95@gmail.com>
2020-04-13 14:32:36 +0200

    :art: address field -> street, city, postal code fields :construction: Input control WIP: onChangeFieldLocation action added

--- oreijin-frontend/src/containers/Register.js
+++ oreijin-frontend/src/containers/Register.js
@@ -1,15 +1,19 @@
 import { connect } from 'react-redux';
 import Register from '../components/Visitor/Register';
 
-import { onChangeField, onToggleTC } from '../actions/register';
+import { onChangeField, onToggleTC, onChangeFieldLocation } from '../actions/register';
 
 const mapStateToProps = (state) => ({
   form: state.register.form,
+  location: state.register.location,
   isTCChecked: state.register.isTCChecked,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   onChangeField: (inputName, inputValue) => dispatch(onChangeField({
+    [inputName]: inputValue,
+  })),
+  onChangeFieldLocation: (inputName, inputValue) => dispatch(onChangeFieldLocation({
     [inputName]: inputValue,
   })),
   onToggleTC: () => dispatch(onToggleTC()),
