f528cfd1179fb8ca05434a8da5e8ba2584df78e4
Jonathan Raso <jonathanraso.dev@gmail.com>
2020-04-13 10:02:15 +0200

    :tada: Form with inputs for login page add

--- oreijin-frontend/src/components/Visitor/Connexion/index.js
+++ oreijin-frontend/src/components/Visitor/Connexion/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { Button, Form } from 'semantic-ui-react';
+
 import './styles.scss';
 
 const Connexion = () => (
-  <div>
-    Connexion page
-  </div>
+
+  <Form success>
+    <Form.Input type="email" name="email" label="Email" placeholder="Entrez votre adresse mail" />
+    <Form.Input type="password" name="password" label="Mot de passe" placeholder="Entrez votre mot de passe" />
+    <Button>Connexion</Button>
+  </Form>
+
 );
 
 export default Connexion;
