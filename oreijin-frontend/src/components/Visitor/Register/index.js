8071f0b33dc313dcd95e7b6e0124e1c3941d372b
Bryan M <allen.bry95@gmail.com>
2020-04-13 10:56:44 +0200

    :construction: Geocoder WIP: Geocoder input added, value props null bug to fix

--- oreijin-frontend/src/components/Visitor/Register/index.js
+++ oreijin-frontend/src/components/Visitor/Register/index.js
@@ -1,5 +1,6 @@
 // == Import npm
 import React from 'react';
+import Geocoder from 'react-mapbox-gl-geocoder';
 import {
   Header, Container, Segment, Form,
 } from 'semantic-ui-react';
@@ -14,15 +15,17 @@ const Register = () => (
       <Header as="h1" dividing textAlign="center">Inscription</Header>
       <Form>
         <Form.Group>
-          <Form.Field required width={8}>
+          <Form.Field width={8}>
             <Form.Input
+              required
               label="Prénom"
               placeholder="Prénom"
               type="text"
             />
           </Form.Field>
-          <Form.Field required width={8}>
+          <Form.Field width={8}>
             <Form.Input
+              required
               label="Nom"
               placeholder="Nom"
               type="text"
@@ -30,8 +33,9 @@ const Register = () => (
           </Form.Field>
         </Form.Group>
         <Form.Group>
-          <Form.Field required width={10}>
+          <Form.Field width={10}>
             <Form.Input
+              required
               label="Pseudo"
               placeholder="Pseudo"
               type="text"
@@ -39,42 +43,49 @@ const Register = () => (
           </Form.Field>
           <Form.Field required width={6}>
             <Form.Input
+              required
               label="Date de naissance"
               placeholder="Date de naissance"
               type="date"
             />
           </Form.Field>
         </Form.Group>
-        <Form.Field required>
+        <Form.Field>
           <Form.Input
+            required
             label="Email"
             placeholder="Email"
             type="email"
           />
         </Form.Field>
-        <Form.Field required>
+        <Form.Field>
           <Form.Input
+            required
             label="Mot de passe"
             placeholder="Mot de passe"
             type="password"
           />
         </Form.Field>
-        <Form.Field required>
+        <Form.Field>
           <Form.Input
+            required
             label="Vérification du mot de passe"
             placeholder="Vérification du mot de passe"
             type="password"
           />
         </Form.Field>
-        <Form.Field required>
-          <Form.Input
-            label="Adresse"
-            placeholder="Adresse"
-            type="text"
-          />
-        </Form.Field>
-        <Form.Field required>
-          <Form.Checkbox label="I agree to the Terms and Conditions" />
+        <Form.Field
+          required
+          control={Geocoder}
+          label="Adresse Postale"
+          viewport={{}}
+          mapboxApiAccessToken="pk.eyJ1Ijoibm91Z2F6YWtpIiwiYSI6ImNrOG9uaG90NjA0MWEzZ242OWY5Z3o2ZGoifQ.lMw3p6r7TW0oBoxfMrzpFA"
+          onSelected={(viewport, item) => {
+            console.log(viewport, item);
+          }}
+        />
+        <Form.Field>
+          <Form.Checkbox required label="I agree to the Terms and Conditions" />
         </Form.Field>
         <Form.Button type="submit">Soumettre</Form.Button>
       </Form>
