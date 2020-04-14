ffd39a3b1225703a59f87377691194872d0e313c
Jonathan Raso <jonathanraso.dev@gmail.com>
2020-04-14 10:17:44 +0200

    :construction: login form page without request done

--- oreijin-frontend/src/middleware/authMiddleware.js
+++ oreijin-frontend/src/middleware/authMiddleware.js
@@ -1,12 +1,27 @@
-// Install axios library later ---> import axios from 'axios';
+import axios from 'axios';
 
 import { LOGIN, LOGOUT } from '../actions/user';
 
 export default (store) => (next) => (action) => {
   switch (action.type) {
     case LOGIN:
-      console.log('bouton connexion cliqué');
-      console.log(store.getState().user.form);
+      axios({
+        method: 'post',
+        url: 'http:localhost:8001/api/login_check',
+        withCredentials: true,
+        data: {
+          username: store.getState().user.form.username,
+          password: store.getState().user.form.password,
+        },
+      })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      /* console.log('bouton connexion cliqué');
+      console.log(store.getState().user.form); */
       break;
     case LOGOUT:
       console.log('bouton déconnexion cliqué');
