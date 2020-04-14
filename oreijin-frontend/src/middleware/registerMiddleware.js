a8c77c492c809c0d08376e7e8c6f7b4f9c67fb2d
julien <julien.devone@gmail.com>
2020-04-14 18:35:56 +0200

    :bug: Correction about url http to https CORS-error

--- oreijin-frontend/src/middleware/registerMiddleware.js
+++ oreijin-frontend/src/middleware/registerMiddleware.js
@@ -24,7 +24,7 @@ const registerMiddleware = (store) => (next) => (action) => {
           }));
 
           axios({
-            url: "http://localhost:8001/register",
+            url: "https://localhost:8001/register",
             method: 'post',
             data: {
               ...store.getState().register.form,
