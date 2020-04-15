e0897491c8d5051cdf060fc5d220537ee71b529d
Jonathan Raso <jonathanraso.dev@gmail.com>
2020-04-15 10:39:05 +0200

    :construction: home connected layout with one side for services and one side for mapping

--- oreijin-frontend/src/components/Home/HomeConnected/index.js
+++ oreijin-frontend/src/components/Home/HomeConnected/index.js
@@ -1,14 +1,24 @@
 // == Import npm
 import React from 'react';
 // == Import components that we need to use from Semantic-UI-React
-import { Container } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 // == Import styles and assets
-// import './styles.scss';
+import './styles.scss';
 
 // == Component
 const HomeConnected = () => (
-  <Container>Home Connected</Container>
+  <Grid className="home__connected" columns={2} divided>
+    <Grid.Column className="home__connected__services" textAlign="center" mobile={16} tablet={9} computer={9}>
+      <div className="home__connected__services__filter">Emplacement des filtres</div>
+      <h2>ANNONCES</h2>
+      <div>Emplacement des différentes annonces</div>
+    </Grid.Column>
+    <Grid.Column textAlign="center" mobile={16} tablet={7} computer={7}>
+      <h2>MAP</h2>
+      <div>Emplacement de la map</div>
+    </Grid.Column>
+  </Grid>
 
 );
 
