f1de0cfcd5d7a0ea1bebdf30ae0dc9bce94cd10c
Bryan M <allen.bry95@gmail.com>
2020-04-18 19:05:22 +0200

    :art: Service details mobile, tablet, and desktop OK

--- oreijin-frontend/src/components/Member/ServiceDetails/index.js
+++ oreijin-frontend/src/components/Member/ServiceDetails/index.js
@@ -1,54 +1,81 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import {
-  Container, Header, Segment, Form, TextArea, Feed, Grid, Icon, Label, Image, Divider, Button,
+  Container, Header, Segment, Form, TextArea, Feed,
+  Label, Image, Divider, Button, Grid, Item, Rating,
 } from 'semantic-ui-react';
 
 import './style.scss';
 import logo from '../../../assets/images/logo.png';
 
 const ServiceDetails = ({ /* service */ }) => {
-  /*console.log(service);*/
+  // console.log(service);
   return (
-    <Container>
-      <Segment textAlign="center">
-        <Header as="h1">{/*service.title*/}Titre du Service</Header>
-        <Container>
-          <Label>
-            Proposition
-          </Label>
-          <Label>
-            Jardinage
-          </Label>
-        </Container>
-        <Container className="service__details__avatar">
-          <Image src={logo} size="tiny" />
-          <p>Nom du Membre</p>
-          <p>Code Postal - Ville</p>
-        </Container>
-        <Divider horizontal>
-          <Header as="h5">
-            Description
-          </Header>
-        </Divider>
-        <Container>
-          <p>Texte détaillé de description du service proposé ou demandé</p>
-        </Container>
-        <Divider horizontal>
-          <Header as="h5">
-            Photo
-          </Header>
-        </Divider>
-        <Container>
+    <Segment as={Container} className="service">
+      <Grid container centered stackable>
+        <Grid.Row>
+          <Header style={{ fontSize: '1.5rem' }} as="h1" sub>Titre du Service</Header>
+          <Container style={{ marginTop: '0.7rem' }}>
+            <Label>
+              Proposition
+            </Label>
+            <Label>
+              Jardinage
+            </Label>
+          </Container>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={8} as={Item.Group}>
+            <Item>
+              <Item.Image src={logo} size="small" circular />
+              <Item.Content className="service__user__content">
+                <Item.Header>Nom du Membre</Item.Header>
+                <Item.Extra>
+                  <Label>
+                  Fiabilité : <Rating icon="star" defaultRating={1} maxRating={5} />
+                  </Label>
+                </Item.Extra>
+                <Item.Meta>Code Postal - Ville</Item.Meta>
+              </Item.Content>
+            </Item>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Divider horizontal>
+        <Header as="h5">
+          Description
+        </Header>
+      </Divider>
+
+      <Grid>
+        <Grid.Row>
+          <Grid.Column as={Container} textAlign="left" className="padding--left--right">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet elit et erat placerat, et fermentum felis malesuada. Vestibulum id nulla blandit, ornare velit sit amet, malesuada urna. Sed hendrerit viverra elit, eu imperdiet mi rhoncus eget. Vestibulum gravida sed turpis et commodo. Suspendisse potenti. Proin ac dolor rutrum, suscipit lorem eu, fringilla ante.
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
+      <Divider horizontal>
+        <Header as="h5">
+          Photo
+        </Header>
+      </Divider>
+
+      <Grid centered>
+        <Grid.Row>
           <Image src={logo} />
-        </Container>
-        <Divider horizontal>
-          <Header as="h5">
-            Réponses
-          </Header>
-        </Divider>
-        <Container>
-          <Feed>
+        </Grid.Row>
+      </Grid>
+
+      <Divider horizontal>
+        <Header as="h5">
+          Réponses
+        </Header>
+      </Divider>
+
+      <Grid>
+        <Grid.Row>
+          <Grid.Column as={Feed} className="padding--left--right">
             <Feed.Event>
               <Feed.Label image={logo} />
               <Feed.Content>
@@ -73,21 +100,25 @@ const ServiceDetails = ({ /* service */ }) => {
                 </Feed.Extra>
               </Feed.Content>
             </Feed.Event>
-          </Feed>
-        </Container>
-        <Divider horizontal>
-          <Header as="h5">
-            Se proposer
-          </Header>
-        </Divider>
-        <Container>
-          <Form>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
+      <Divider horizontal>
+        <Header as="h5">
+          Se proposer
+        </Header>
+      </Divider>
+
+      <Grid>
+        <Grid.Row>
+          <Grid.Column as={Form}>
             <TextArea placeholder="Envoyer une réponse" />
             <Button className="service__details__button" onClick={console.log('coucou')}>Envoyer</Button>
-          </Form>
-        </Container>
-      </Segment>
-    </Container>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   );
 };
 
