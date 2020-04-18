261e24eb9507284584e7f44408f515dd21ea3788
JonathanRaso <57390276+JonathanRaso@users.noreply.github.com>
2020-04-18 19:00:30 +0200

    :white_check_mark: profil page layout done, getting infos from dynamic props. Still needs some adjustements (#27)

--- oreijin-frontend/src/components/Member/Profil/index.js
+++ oreijin-frontend/src/components/Member/Profil/index.js
@@ -1,8 +1,70 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import {
+  Container, Header, Segment, Form, TextArea, Feed, Icon, Label, Image, Divider, Button,
+} from 'semantic-ui-react';
 
-const Profil = () => (
-  <Container text> Profil - Work in progress ...</Container>
-);
+import logo from '../../../assets/images/logo.png';
+import './styles.scss';
+
+const Profil = ({ userInfos }) => {
+  console.log(userInfos);
+  return (
+    <Container>
+      <Segment textAlign="center">
+        <Header as="h1">Profil</Header>
+        <Container className="service__details__avatar">
+          <Image src={logo} size="small" />
+          <Button className="profil__import__button" size="small" color="blue">Importer une photo</Button>
+        </Container>
+        <Container className="profil__name">
+          <p>Prénom : <span>{userInfos.firstname} </span></p>
+          <p>Nom : <span>{userInfos.lastname} </span></p>
+        </Container>
+        <Divider horizontal>
+          <Header as="h5">
+            Infos
+          </Header>
+        </Divider>
+        <Container>
+          <Form>
+            <Form.Field>
+              <label>Prénom</label>
+              <input placeholder={userInfos.firstname} />
+            </Form.Field>
+            <Form.Field>
+              <label>Nom</label>
+              <input placeholder={userInfos.lastname} />
+            </Form.Field>
+            <Form.Field>
+              <label>Email</label>
+              <input placeholder={userInfos.email} />
+            </Form.Field>
+            <Button className="profil__change__button" size="small" type="submit">Modifier</Button>
+          </Form>
+        </Container>
+        <Divider horizontal>
+          <Header as="h5">
+            Suppression
+          </Header>
+        </Divider>
+        <Container>
+          <p>Attention</p>
+          <p>La suppression de votre compte est définitive. Vous perdrez toutes les informations et les services liés à ce compte.</p>
+          <Button className="profil__delete__button" size="small" color="red">Supprimer le compte</Button>
+        </Container>
+      </Segment>
+    </Container>
+  );
+};
+
+Profil.propTypes = {
+  userInfos: PropTypes.shape({
+    email: PropTypes.string,
+    firstname: PropTypes.string,
+    lastname: PropTypes.string,
+  }).isRequired,
+};
+
 
 export default Profil;
