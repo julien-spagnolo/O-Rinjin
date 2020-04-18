3559cf5588141543fe21ae9671ef19965e40362a
Bryan M <allen.bry95@gmail.com>
2020-04-18 12:35:26 +0200

    :construction: Service details page: first page visual, Bug: data lost on refresh

--- oreijin-frontend/src/components/Member/ServiceDetails/index.js
+++ oreijin-frontend/src/components/Member/ServiceDetails/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container } from 'semantic-ui-react';
+import { Container, Header, Segment, Form, TextArea, Feed, Icon } from 'semantic-ui-react';
+
+import logo from '../../../assets/images/logo.png';
 
 const ServiceDetails = ({ service }) => {
   console.log(service);
-
   return(
-    <Container text>Service Details - {service.id} {service.title}</Container>
+    <Container text>
+      <Segment>
+      <Header as="h1">{service.title}</Header>
+      Service Details - {service.id} {service.title}
+      <Feed>
+        <Feed.Event>
+          <Feed.Label image={logo} />
+          <Feed.Content>
+            <Feed.Summary>
+              <a>Nom du membre</a>
+              <Feed.Date>Date de l'envoi du message</Feed.Date>
+            </Feed.Summary>
+            <Feed.Extra text>
+              Réponse à un service pour prendre contact avec la personne
+            </Feed.Extra>
+          </Feed.Content>
+        </Feed.Event>
+      </Feed>
+      <Form>
+        <TextArea placeholder="Répondre à l'annonce" />
+      </Form>
+      </Segment>
+    </Container>
   );
 };
 
