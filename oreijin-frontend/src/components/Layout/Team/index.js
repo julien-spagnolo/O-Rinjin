aa161d9f3f4d1fa02f6a455c15640ed1801e4622
Jonathan Raso <jonathanraso.dev@gmail.com>
2020-04-18 10:07:45 +0200

    :tada: team page layout ok, need to fix size of cards

--- oreijin-frontend/src/components/Layout/Team/index.js
+++ oreijin-frontend/src/components/Layout/Team/index.js
@@ -1,15 +1,96 @@
 // == Import npm
 import React from 'react';
 
+import {
+  Header, Container, Card, Icon, Image,
+} from 'semantic-ui-react';
 // == Import
 
 import './styles.scss';
+import logo from '../../../assets/images/logo.png';
 
 // == Composant
 const Team = () => (
-  <div>
-   Team
-  </div>
+  <Container>
+    <Header as="h2" textAlign="center">Notre équipe</Header>
+    <Card.Group stackable itemsPerRow={2}>
+      <Card raised>
+        <Image src={logo} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Hélène</Card.Header>
+          <Card.Meta>
+            Dev Backend/Symfony
+          </Card.Meta>
+          <Card.Description>
+            Elle fait partie du duo de développeurs backend qui s'occupe de tout ce qui touche à Symfony coté serveur
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href="https://fr.linkedin.com/">
+            <Icon name="linkedin" />
+            Me contacter
+          </a>
+        </Card.Content>
+      </Card>
+      <Card raised>
+        <Image src={logo} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Julien</Card.Header>
+          <Card.Meta>
+            Lead Dev Backend/Symfony
+          </Card.Meta>
+          <Card.Description>
+            Il fait partie du duo de développeurs backend qui s'occupe de tout ce qui touche
+            à Symfony coté serveur.
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href="https://fr.linkedin.com/">
+            <Icon name="linkedin" />
+            Me contacter
+          </a>
+        </Card.Content>
+      </Card>
+      <Card raised>
+        <Image src={logo} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Bryan</Card.Header>
+          <Card.Meta>
+            Lead Dev Frontend/React
+          </Card.Meta>
+          <Card.Description>
+            Il fait partie du duo de développeurs frontend qui s'occupe de tout ce qui touche
+            à React et au coté client.
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href="https://fr.linkedin.com/">
+            <Icon name="linkedin" />
+            Me contacter
+          </a>
+        </Card.Content>
+      </Card>
+      <Card raised>
+        <Image src={logo} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Jonathan</Card.Header>
+          <Card.Meta>
+            Dev Backend/Symfony
+          </Card.Meta>
+          <Card.Description>
+            Il fait partie du duo de développeurs frontend qui s'occupe de tout ce qui touche
+            à React et au coté client.
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href="https://fr.linkedin.com/">
+            <Icon name="linkedin" />
+            Me contacter
+          </a>
+        </Card.Content>
+      </Card>
+    </Card.Group>
+  </Container>
 );
 
 // == Export
