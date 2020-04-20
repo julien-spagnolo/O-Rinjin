655ef0dbdf4f2e6021a00f5b54e916705fa48ef8
JonathanRaso <57390276+JonathanRaso@users.noreply.github.com>
2020-04-20 09:41:19 +0200

    :white_check_mark: markers and popup with infos done, getting datas from js files. Waiting for api route (#31)

---
+++ oreijin-frontend/src/map-services.js
@@ -0,0 +1,49 @@
+const mapServices = [
+  {
+    key: 1,
+    imgKey: 7,
+    latitude: 48.866667,
+    longitude: 2.333333,
+    title: 'Besoin de couper un arbre',
+    category: 'Jardinage',
+    type: 'Demande',
+  },
+  {
+    key: 2,
+    imgKey: 8,
+    latitude: 48.866456,
+    longitude: 2.4,
+    title: 'Surveiller mes enfants après les cours',
+    category: 'Babysitting',
+    type: 'Demande',
+  },
+  {
+    key: 3,
+    imgKey: 9,
+    latitude: 48.80987,
+    longitude: 2.3234,
+    title: 'Trajet entre le 18ème et le 15ème',
+    category: 'Covoiturage',
+    type: 'Proposition',
+  },
+  {
+    key: 4,
+    imgKey: 10,
+    latitude: 48.8347,
+    longitude: 2.3675,
+    title: 'Donne vêtements pour enfants',
+    category: 'Don',
+    type: 'Proposition',
+  },
+  {
+    key: 5,
+    imgKey: 11,
+    latitude: 48.89990,
+    longitude: 2.3675,
+    title: 'Prendre en charge mon chat durant mes vacances',
+    category: 'Animaux',
+    type: 'Demande',
+  },
+];
+
+export default mapServices;
