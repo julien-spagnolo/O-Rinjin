841d10558e4b2291021c1b5edbcd304a3105614b
Jonathan Raso <jonathanraso.dev@gmail.com>
2020-04-25 17:10:05 +0200

    :sparkles: form validation for comments done

--- oreijin-frontend/src/reducers/comments.js
+++ oreijin-frontend/src/reducers/comments.js
@@ -1,8 +1,10 @@
-import { ON_CHANGE_FIELD_REPLY, ADD_COMMENT_SUCCESS } from '../actions/comments';
+import { ON_CHANGE_FIELD_REPLY, ADD_COMMENT_SUCCESS, ADD_COMMENT_ERROR } from '../actions/comments';
+import { REPLY_FORM_ERROR } from '../actions/form';
 
 const initialState = {
   list: [],
   reply: '',
+  isError: false,
 };
 
 export default (state = initialState, action = {}) => {
@@ -11,7 +13,13 @@ export default (state = initialState, action = {}) => {
       return {
         ...state,
         reply: '',
+        isError: false,
       };
+    case REPLY_FORM_ERROR:
+      return {
+        ...state,
+        isError: true,
+      }
     case ON_CHANGE_FIELD_REPLY:
       return {
         ...state,
